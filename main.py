2d645310b0f9996221ce2334daa103ffce309946
fujiapp <8_carpboy_74@au.com>
2024-07-02 13:42:12 +0900

    情報を随時アプロードに変更

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from datetime import datetime
 import pytz
+import aiofiles
+from aiocsv import AsyncDictWriter
 # fast app
 app = FastAPI()
 # 認証情報の設定
@@ -50,12 +52,41 @@ notification = notification_generator()
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
+    csv_data = {"timestamp": [], "data_type": [], "heart_rate": [], "x": [], "y": [], "z": [], "rpe": []}
     clients.append(websocket)
     try:
         while True:
             data = await websocket.receive_text()
-            await save_to_drive(data)  # Google Driveに保存
+
             await notification.asend(data)
+            data_dict = json.loads(data)
+
+            csv_data["timestamp"].append(data_dict["timestamp"])
+            csv_data["data_type"].append(data_dict["data_type"])
+            if data_dict["data_type"] == "finish":
+                await save_to_drive(csv_data)  # Google Driveに保存
+                csv_data = {"timestamp": [], "data_type": [], "heart_rate": [], "x": [], "y": [], "z": [], "rpe": []}
+                continue
+
+            # 初期化
+            fields = ["x", "y", "z", "heart_rate", "rpe"]
+            data_defaults = {field: None for field in fields}
+
+            # 各データタイプごとの値を設定
+            if data_dict["data_type"] == "accelerometer":
+                data_defaults.update({
+                    "x": data_dict["data"]["x"],
+                    "y": data_dict["data"]["y"],
+                    "z": data_dict["data"]["z"]
+                })
+            elif data_dict["data_type"] == "heart_rate":
+                data_defaults["heart_rate"] = data_dict["data"]["heartRate"]
+            elif data_dict["data_type"] == "rpe":
+                data_defaults["rpe"] = data_dict["data"]["rpe"]
+
+            for field in fields:
+                csv_data[field].append(data_defaults[field])
+
     except WebSocketDisconnect:
         clients.remove(websocket)
 
@@ -64,9 +95,14 @@ async def websocket_endpoint(websocket: WebSocket):
 # CSVデータをGoogle Driveに保存する関数
 
 
-async def save_to_drive(data: str):
+async def save_to_drive(csv_data: dict):
     async with aiofiles.tempfile.NamedTemporaryFile('w', delete=False) as temp_file:
-        await temp_file.write(data)
+        # CSVライターを作成
+        writer = AsyncDictWriter(temp_file, fieldnames=csv_data.keys())
+        await writer.writeheader()
+        rows = [dict(zip(csv_data, t)) for t in zip(*csv_data.values())]
+        await writer.writerows(rows)
+
         temp_file_path = temp_file.name
     tokyo_tz = pytz.timezone('Asia/Tokyo')
     current_time = datetime.now(tokyo_tz).strftime("%y%m%d-%H%M")
